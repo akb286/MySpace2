e4ea1ced1dca3d2dbcdbf22c797f6f4ccb3f430d
Alicia Baker <aliciabaker314@gmail.com>
2018-03-16 17:21:12 -0600

    version 2

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,33 @@
+class Api::PostsController < ApplicationController
+
+  def index
+    @posts = current_user.posts.all
+    render json: @posts
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post
+    else
+      message = "Not working"
+      render json: message
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(title:, :body, :user_id)
+  end
+
+end
